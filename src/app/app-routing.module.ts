b64efd5091e339ae83d771c4155322494fd84835
StevenPerezM <edwin.perez@usantoto.edu.co>
2021-07-12 13:59:25 -0500

    feat: add base routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BoardComponent} from "./components/board/board.component";
+import {RegisterComponent} from "./components/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'game/:id', component: BoardComponent },
+  { path: '', component: RegisterComponent },
+  //{ path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
